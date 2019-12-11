79a442ed1a0cd4ee3e5531f835fa663ae3ad4bf4
kawallala <martin.araya.zavala@gmail.com>
2019-12-11 16:19:35 -0400

    added test for CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,16 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CaesarShiftCipherTest {
+  @ParameterizedTest(name = "string = {0}, shift = {1}, result = {2}")
+  @CsvSource({ "abc,3,def"})
+  public void CipherTest(String string, int shift, String res){
+    String result = new CaesarShiftCipher().CaesarShiftCipher(string,shift);
+    assertEquals(res,result);
+  }
 }
